a6f45da5e425639894f93bd43514cb278b1f86fd
Tory <88531407+1two13@users.noreply.github.com>
2023-05-14 22:18:36 +0900

    chore: 파일 확장자 ts, tsx로 변경

--- src/components/InputTodo.tsx
+++ src/components/InputTodo.tsx
@@ -1,11 +1,12 @@
-import { FaPlusCircle, FaSpinner } from "react-icons/fa";
-import { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from 'react';
+import { FaPlusCircle, FaSpinner } from 'react-icons/fa';
 
-import { createTodo } from "../api/todo";
-import useFocus from "../hooks/useFocus";
+import { createTodo } from '../api/todo';
+import useFocus from '../hooks/useFocus';
+import { InputTodoProps, Todo } from '../types/types';
 
-const InputTodo = ({ setTodos }) => {
-  const [inputText, setInputText] = useState("");
+const InputTodo = ({ setTodos }: InputTodoProps) => {
+  const [inputText, setInputText] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const { ref, setFocus } = useFocus();
 
@@ -14,31 +15,31 @@ const InputTodo = ({ setTodos }) => {
   }, [setFocus]);
 
   const handleSubmit = useCallback(
-    async (e) => {
+    async (e: React.FormEvent<HTMLFormElement>) => {
       try {
         e.preventDefault();
         setIsLoading(true);
 
         const trimmed = inputText.trim();
         if (!trimmed) {
-          return alert("Please write something");
+          return alert('Please write something');
         }
 
         const newItem = { title: trimmed };
         const { data } = await createTodo(newItem);
 
         if (data) {
-          return setTodos((prev) => [...prev, data]);
+          return setTodos((prev: Todo[]) => [...prev, data]);
         }
       } catch (error) {
         console.error(error);
-        alert("Something went wrong.");
+        alert('Something went wrong.');
       } finally {
-        setInputText("");
+        setInputText('');
         setIsLoading(false);
       }
     },
-    [inputText, setTodos],
+    [inputText, setTodos]
   );
 
   return (

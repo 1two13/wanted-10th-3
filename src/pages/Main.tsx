9f7b9c241d4cc65899af4c392bd5f2307f25a8cc
Tory <88531407+1two13@users.noreply.github.com>
2023-05-17 23:14:11 +0900

    feat: useContext 값 추가, 컴포넌트 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,13 +3,17 @@ import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import InputTodo from '../components/InputTodo';
 import TodoList from '../components/TodoList';
+import Dropdown from '../components/Dropdown';
+import { Todo } from '../types/todoTypes';
+import { SearchedData } from '../types/searchTypes';
 import { getTodoList } from '../api/todo';
-import { Todo } from '../types/types';
-import { TodoListContext } from '../hooks/useContext';
+import { ContextList } from '../hooks/useContext';
 import '../css/main.css';
 
 const Main = () => {
   const [todoListData, setTodoListData] = useState<Todo[]>([]);
+  const [searchedData, setSearchedData] = useState<SearchedData | undefined>();
+  const [inputText, setInputText] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -19,15 +23,25 @@ const Main = () => {
   }, [setTodoListData]);
 
   return (
-    <TodoListContext.Provider value={{ todoListData, setTodoListData }}>
+    <ContextList.Provider
+      value={{
+        todoListData,
+        setTodoListData,
+        searchedData,
+        setSearchedData,
+        inputText,
+        setInputText,
+      }}
+    >
       <div className="container">
         <div className="inner">
           <Header />
           <InputTodo />
+          <Dropdown />
           <TodoList />
         </div>
       </div>
-    </TodoListContext.Provider>
+    </ContextList.Provider>
   );
 };
 

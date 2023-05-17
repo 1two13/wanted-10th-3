03cd05c433fbdda290b6bcf21ded74f23d4a71e9
Tory <88531407+1two13@users.noreply.github.com>
2023-05-17 23:10:44 +0900

    feat: 상수 파일 생성

---
+++ src/static/constants.ts
@@ -0,0 +1,5 @@
+const ERROR_ALERT_MESSAGE = 'Something went wrong.';
+const EMPTY_ALERT_MESSAGE = 'Please write something';
+const DEBOUNCE_LIMIT_TIME = 500;
+
+export { ERROR_ALERT_MESSAGE, EMPTY_ALERT_MESSAGE, DEBOUNCE_LIMIT_TIME };
